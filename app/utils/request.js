06defeffe8b38500e85eb56894b76bae4cfe092f
guyZz <starkcoder@sina.com>
2019-05-23 22:13:33 +0800

    redux-persist

--- app/utils/request.js
+++ app/utils/request.js
@@ -1,11 +1,37 @@
-import {WEATHER_URL} from '../constant/config';
+import {WEATHER_URL, WEATHER_KEY} from '../constant/config';
 
 /**
  * fetch 数据需要 json() 转化为json数据
  *
  * */
-export default async function request(uri, method, params) {
 
+async function requestWeatherData(cityName) {
+  if (!cityName) {
+    return new Error('cityName为空');
+  }
+  let weatherList = [];
+  await fetch(WEATHER_URL + "?city=" + cityName + "&key=" + WEATHER_KEY, {method: 'GET'})
+    .then(res => {
+      res.json().then(res => {
+        weatherList = res.result?.future;
+        if (weatherList) {
+          console.info('[request]weather', weatherList)
+        } else {
+          console.info('[getWeather]', '当前天气数据空')
+          return new Error('ERROR')
+        }
+      }, err => {
+        console.info('errjson', err)
+        return new Error('ERROR')
+      })
+    }, err => {
+      console.info('err', err)
+      return new Error('ERROR')
+    })
+}
+
+
+export default async function request(uri, method, params) {
 
   fetch(uri, {
     method: method,
@@ -25,4 +51,8 @@ export default async function request(uri, method, params) {
     }
   )
 
+}
+
+export {
+  requestWeatherData,
 }
