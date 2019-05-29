4c45c72b223f4d6b266a199e2079acbfed5e10d6
guyZz <starkcoder@sina.com>
2019-05-29 22:27:41 +0800

    today history

--- app/views/history/History.js
+++ app/views/history/History.js
@@ -1,3 +1,55 @@
+// import React, {PureComponent} from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   FlatList,
+//   TouchableOpacity,
+//   Image,
+// } from 'react-native';
+//
+// //多选框
+// import Toolbar from '../../component/header/Toolbar';
+// import SwipeableItem from '../../component/swipeable/SwipeableItem';
+// import Swipeable from 'react-native-swipeable';
+// import {connect} from 'react-redux';
+//
+// class History extends PureComponent {
+//
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   componentWillMount(): void {
+//   }
+//
+//   render() {
+//     const DATA = this.props.historyInfoList?.[2];
+//     console.info('pic', DATA.pic)
+//     return (
+//       <View>
+//         <Toolbar
+//           title={'历史上的今天'}
+//         />
+//         <View>
+//           <Image
+//             source={{uri: DATA.pic}}
+//             style={{width: 80, height: 80}}
+//           />
+//           <Text>{DATA.title}</Text>
+//           <Text>{DATA.des}</Text>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+//
+// export default connect(state => ({
+//   historyInfoList: state.constant.historyInfoList
+// }), {})(History)
+
+
 import React, {PureComponent} from 'react';
 import {
   Platform,
@@ -9,42 +61,67 @@ import {
   Image,
 } from 'react-native';
 
-//多选框
-import Toolbar from '../../component/header/Toolbar';
-import SwipeableItem from '../../component/swipeable/SwipeableItem';
-import Swipeable from 'react-native-swipeable';
-import {connect} from 'react-redux';
+import {createDrawerNavigator, createAppContainer} from 'react-navigation';
 
-class History extends PureComponent {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentWillMount(): void {
-  }
+class MyHomeScreen extends PureComponent {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+    drawerIcon: ({tintColor}) => (
+      <Image
+        source={require('../../constant/image/circle_black_in_32px.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
 
   render() {
-    const DATA = this.props.historyInfoList?.[2];
-    console.info('pic', DATA.pic)
     return (
-      <View>
-        <Toolbar
-          title={'历史上的今天'}
-        />
-        <View>
-          <Image
-            source={{uri: DATA.pic}}
-            style={{width: 80, height: 80}}
-          />
-          <Text>{DATA.title}</Text>
-          <Text>{DATA.des}</Text>
-        </View>
-      </View>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('Notifications')}
+      >
+        <Text>Go to notifications</Text>
+      </TouchableOpacity>
     );
   }
 }
 
-export default connect(state => ({
-  historyInfoList: state.constant.historyInfoList
-}), {})(History)
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Notifications',
+    drawerIcon: ({tintColor}) => (
+      <Image
+        source={require('../../constant/image/circle_check_32px.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.goBack()}
+      >
+        <Text>Go back home</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+export default MyApp;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {AppRegistry} from 'react-native';
 // import HomePage from './app/views/home/HomePage';
 import {name as appName} from './app.json';
 // import SimpleApp from './app/navigation/index';
-import SimpleApp from './app/index';
+// import SimpleApp from './app/index';
+import SimpleApp from './app/views/history/History';
 
 AppRegistry.registerComponent(appName, () => SimpleApp);
